426764fbd11c66c430f5056eddbe6d38b187a9ab
Obua Stella <stellaobua123@gmail.com>
2024-02-12 06:39:27 +0100

    Fixed dependency issue

--- react-chartjs/src/App.jsx
+++ react-chartjs/src/App.jsx
@@ -1,9 +1,27 @@
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar, Doughnut, Line } from "react-chartjs-2";
+
 import "./App.css";
+
 const App = () => {
   return (
     <div className="App">
-      <div className="dataCard revenueCard">Chart 1</div>
-      <div className="dataCard customerCard">Chart 2</div>
+      <div className="dataCard revenueCard">
+        Chart 1<div></div>
+      </div>
+      <div className="dataCard customerCard">
+        <Bar
+          data={{
+            labels: ["A", "B", "C"],
+            datasets: [
+              {
+                label: "Revenue",
+                data: [200, 300, 400],
+              },
+            ],
+          }}
+        />
+      </div>
       <div className="dataCard categoryCard">Chart 3</div>
     </div>
   );
